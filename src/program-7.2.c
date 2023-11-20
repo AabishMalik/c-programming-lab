8668a1f3b308956b574d68626d3a5bf3147e60ac
opticXC <aabishmalik018@gmail.com>
2023-11-20 19:28:17 +0530

    updates to 7.2

--- src/program-7.2.c
+++ src/program-7.2.c
@@ -6,39 +6,22 @@
 #include <stdlib.h>
 
 int main(){
-    int A[3][3] = {
-        {1,2,3},
-        {4,5,6},
-        {7,8,9},
-    };
-
-    int B[3][3] = {
-        {9,8,7},
-        {6,5,4},
-        {3,2,1},
-    };
-    int m1 = sizeof(A)/sizeof(A[0]);
-    int n1 = sizeof(A[0])/sizeof(A[0][0]);
-    int m2 = sizeof(B)/sizeof(B[0]);
-    int n2 = sizeof(B[0])/sizeof(B[0][0]);
-    
-    if (m2 != n1){
-        printf("The given matricies cannot be multiplied\n");
-        exit(1);
-    }
+    int m1=3,n2=3;
+    int A[3][3] = { {1,2,3}, {4,5,6}, {7,8,9} };
+    int B[3][3] = { {9,8,7}, {6,5,4}, {3,2,1} };
 
     int *C = malloc(sizeof(int) * m1 * n2);
 
     for (int m =0; m<m1; m++){
         for (int n=0; n<n2;n++){
-            for (int i=0;i<n1; i++){
+            for (int i=0;i<n2; i++){
                 *(C + (m*m1) + (n) ) += A[m][i] * B[i][n];
             }
         }
     }
     for (int m=0; m<m1; m++){
         for (int n=0; n<n2; n++){
-            printf("%d%s", *(C+ (m*m1) + (n)) , (n<n2-1)? " ": "");
+            printf("%d ", *(C+ (m*m1) + (n)));
         }
         printf("\n");
     }
