d50620ffbaa766a5a7583089a21fc52ee776d376
opticXC <aabishmalik018@gmail.com>
2023-11-29 19:16:26 +0530

    updates to 9.1

--- src/program-9.1.c
+++ src/program-9.1.c
@@ -1,9 +1,27 @@
 // Program 9.1
 //
 // Calculator using switch cases
-
-
 #include <stdio.h>
+
+int add(int a, int b){
+    return a+b;
+}
+
+int sub(int a, int b){
+    return a-b;
+}
+
+int mul(int a, int b){
+    return a*b;
+}
+
+float div(int a, int b){
+    return (float)a/b;
+}
+
+int mod(int a, int b){
+    return a%b;
+}
 
 int main(){
     int a, b;
@@ -14,19 +32,19 @@ int main(){
     scanf("\n%c", &op);
     switch(op){
         case '+':
-            printf("The sum is %d\n", a+b);
+            printf("The sum is %d\n", add(a,b));
             break;
         case '-':
-            printf("The difference is %d\n", a-b);
+            printf("The difference is %d\n", sub(a,b));
             break;
         case '*':
-            printf("The product is %d\n", a*b);
+            printf("The product is %d\n", mul(a,b));
             break;
         case '/':
-            printf("The quotient is %f\n", (float)a/b);
+            printf("The quotient is %f\n", div(a,b));
             break;
         case '%':
-            printf("The remainder is %d\n", a%b);
+            printf("The remainder is %d\n", mod(a,b));
             break;
         default:
             printf("Invalid operator\n");
